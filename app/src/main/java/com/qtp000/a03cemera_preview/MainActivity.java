7062d49b90da869e8bd12b070438bdb9a71254cc
祁天培 <qtp1300@163.com>
2018-02-24 16:50:54 +0800

    2018.2.24(第二次同步)能正常运行，正常连接摄像头，添加按键，准备调用程序。测试多个commit和一个push提交怎样

--- app/src/main/java/com/qtp000/a03cemera_preview/MainActivity.java
+++ app/src/main/java/com/qtp000/a03cemera_preview/MainActivity.java
@@ -87,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
 //        TextView tv = (TextView) findViewById(R.id.sample_text);
 //        tv.setText(stringFromJNI());
+
         init_btn();
         addlistener();
         search();
