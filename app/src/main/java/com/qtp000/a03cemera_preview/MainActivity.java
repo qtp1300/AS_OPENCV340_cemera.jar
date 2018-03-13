f53998b806bd655e702d9b5798a678e7575bb795
祁天培 <qtp1300@163.com>
2018-03-13 10:50:53 +0800

    18.3.13(第一次同步) 一、从ImageView的cache获得bitmap，将bitmap用于ImageView

--- app/src/main/java/com/qtp000/a03cemera_preview/MainActivity.java
+++ app/src/main/java/com/qtp000/a03cemera_preview/MainActivity.java
@@ -1,10 +1,16 @@
 package com.qtp000.a03cemera_preview;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView_test,imageView_test2;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -17,15 +23,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init_view();
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+
+    private void init_view(){
+        imageView_test = findViewById(R.id.test_imageView);
+        imageView_test2 = findViewById(R.id.test_imageView2);
+        Button btn_copy = findViewById(R.id.btn_getbitmap);
+        btn_copy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap temp;
+                temp = getImageview_test(imageView_test);
+                setImageview_test(temp,imageView_test2);
+
+            }
+        });
+
+
+        imageView_test.setImageResource(R.drawable.test);
+
+    }
+
+    private Bitmap getImageview_test(ImageView imageview){
+        Bitmap getted_image;
+        imageview.setDrawingCacheEnabled(true);    //开启缓存生成Cache
+        getted_image = Bitmap.createBitmap(imageview.getDrawingCache());    //从缓存Cache中获得bitmap
+        imageview.setDrawingCacheEnabled(false);       //关闭缓存，减少内存占用
+        return getted_image;
+    }
+    private void setImageview_test(Bitmap input,ImageView imageview){
+        imageview.setImageBitmap(input);
+
+    }
+
 }
--- openCVLibrary340/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary340/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: F:\\18carcarcarcar\\practice\\03cemera_preview\\openCVLibrary340\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: F:\\18carcarcarcar\\Mine\\Mine\\openCVLibrary340\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
